c9a8791f7d1d62d0338fb212006c790fa1641e2a
Swagnik Dutta <swagnikd@gmail.com>
2025-10-29 20:32:06 +0530

    adds keymap leader tc to preview color themes

--- init.lua
+++ init.lua
@@ -492,6 +492,10 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>tc', function()
+        require('telescope.builtin').colorscheme { enable_preview = true }
+      end, { desc = 'Telescope colorschemes with preview' })
+
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
