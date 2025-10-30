06f6e3fbbc44c3ebdb579767722cff2c0e1c4123
Swagnik Dutta <swagnikd@gmail.com>
2025-10-30 20:27:04 +0530

    adds neoscroll plugin for smooth scrolling

--- init.lua
+++ init.lua
@@ -284,6 +284,13 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup()
+    end,
+  },
+
   --'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   {
     'NMAC427/guess-indent.nvim',
